11a4fa07711340f1feff3859b3e9dbfb7622f30c
utkirbek <utkirbekyuldoshev7@gmail.com>
2024-05-19 00:52:37 +0500

    php database

--- src/Database.php
+++ src/Database.php
@@ -1,5 +1,33 @@
 <?php
 
-class Database{
-    
+class Database
+{
+
+    public $servername;
+    public $database;
+    public $username;
+    public $password;
+
+    public function __construct($servername, $database, $username, $password)
+    {
+        $this->servername = $servername;
+        $this->database = $database;
+        $this->username = $username;
+        $this->password = $password;
+
+        $this->connetc();
+        
+    }
+
+    public function connetc()
+    {
+        try {
+            $conn = new PDO("mysql:host=$this->servername;dbname=$this->database", $this->username, $this->password);
+            // set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+    }
 }
--- src/Post.php
+++ src/Post.php
@@ -1,5 +1,24 @@
 <?php
 
 class Post{
+    public static $pdo;
+    public $id;
+    public $title;
+    public $body;
+    public $create_id;
+
+    // Method to set the PDO instance
+    // public static function setPDO($pdoInstance) {
+    //     self::$pdo = $pdoInstance;
+    // }
+    
+     // Method to get all posts
+     public static function getAll() {
+        $stmt = self::$pdo->prepare("SELECT * FROM posts");
+        $stmt->execute();
+        $posts = $stmt->fetchAll();
+        return $posts;
+    }
+
     
 }
